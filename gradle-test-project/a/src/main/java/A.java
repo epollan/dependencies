5b944a61e68b7050a197bfc066681d276494f28d
Evan Pollan <evan.pollan@gmail.com>
2017-10-04 14:26:26 -0400

    Reformat Java code in sample project

--- gradle-test-project/a/src/main/java/A.java
+++ gradle-test-project/a/src/main/java/A.java
@@ -1,7 +1,7 @@
 import com.google.common.collect.ImmutableList;
 
 public class A {
-	public static void main(String... args) {
-		System.out.println("A");
-	}
+    public static void main(String... args) {
+        System.out.println("A");
+    }
 }
